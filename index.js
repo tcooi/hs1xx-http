6efe9c6db9f6d5f1148536dbea5c0c7fd66996d9
Tjan Chao Ooi <tjanchao.ooi@gmail.com>
2017-01-21 21:47:43 +0100

    add index.js

---
+++ index.js
@@ -0,0 +1,107 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+var Hs100Api = require('hs100-api');
+var config = require('./config.json');
+
+var client = new Hs100Api.Client();
+var plug = client.getPlug({host: config.ip});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8010;
+
+var router = express.Router();
+
+function display(res) {
+  console.log(res);
+}
+
+router.get('/', function(req, res) {
+  getState(function(plug) {
+      res.json(plug);
+    });
+  });
+
+router.route('/state')
+  .get(function(req, res) {
+    getState(function(plug) {
+      res.json(plug);
+    });
+  });
+
+  router.route('/schedule')
+    .get(function(req, res) {
+      getSchedule(function(plug) {
+        res.json(plug);
+      });
+    });
+
+router.route('/on')
+  .get(function(req, res) {
+    on();
+    res.json({state: 'on'});
+  });
+
+router.route('/off')
+  .get(function(req, res) {
+    off();
+    res.json({state: 'off'});
+  });
+
+router.route('/s')
+  .get(function(req, res) {
+    getState(function(plug) {
+      if(plug.relay_state === 1) {
+        off();
+        res.json({state: 'off'});
+      }else {
+        on();
+        res.json({state: 'on'});
+      }
+    });
+  });
+
+function getState(cb) {
+  plug.getInfo().then(function(plug) {
+    display(plug);
+    cb(plug);
+  });
+}
+
+function getSchedule(cb) {
+  plug.getScheduleRules().then(function(plug) {
+    display(plug);
+    cb(plug);
+  });
+}
+
+function on() {
+  plug.setPowerState(true).then(display);
+}
+
+function off() {
+  plug.setPowerState(false).then(display);
+}
+
+function s() {
+  plug.getSysInfo().then(function(plug) {
+    if(plug.relay_state === 1) {
+      off();
+    }else {
+      on();
+    }
+  });
+}
+
+function sc() {
+  plug.getScheduleRules().then(function(plug) {
+    console.log(plug);
+  });
+}
+
+app.use('/hs1xx', router);
+app.listen(port);
+console.log('listening to port ' + port);
