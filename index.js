0228d1880070a3c28b93d6c03142cb7fa4e15c05
tcooi <tjanchao.ooi@gmail.com>
2020-05-12 00:17:21 +0200

    added function to turn on/off specific device

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 
-var TPLINKAPI = require('tplink-smarthome-api');
-var config = require('./config.json');
+const { Client } = require('tplink-smarthome-api');
+const config = require('./config.json');
 
-var client = new TPLINKAPI.Client();
+var client = new Client();
+let devices = [];
+
+console.log('Devices found in config file:')
+
+for (let [key, value] of Object.entries(config)) {
+  console.log(`id: ${key}, ip: ${value}`);
+  devices[key] = client.getPlug({host: value});
+}
+
 var plug = client.getPlug({host: config.a});
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,6 +27,51 @@ var router = express.Router();
 function display(res) {
   console.log(res);
 }
+
+function getState(cb) {
+  plug.getInfo().then(function(state) {
+    display(state);
+    cb(state);
+  });
+}
+
+function getSchedule(cb) {
+  plug.getScheduleRules().then(function(state) {
+    display(state);
+    cb(state);
+  });
+}
+
+function on() {
+  plug.setPowerState(true).then(display);
+}
+
+function off() {
+  plug.setPowerState(false).then(display);
+}
+
+function toggle() {
+  plug.getSysInfo().then(function(state) {
+    if(state.relay_state === 1) {
+      off();
+    }else {
+      on();
+    }
+  });
+}
+
+router.get('/:id/off', function(req, res) {
+  let id = req.params.id
+  devices[id].setPowerState(false).then(display);
+  // console.log(devices[a])
+  })
+
+router.get('/:id/on', function(req, res) {
+  let id = req.params.id
+  devices[id].setPowerState(true).then(display);
+  // console.log(devices[a])
+  })
+
 
 router.get('/', function(req, res) {
   getState(function(plug) {
@@ -64,38 +118,8 @@ router.route('/:id/toggle')
     });
   });
 
-function getState(cb) {
-  plug.getInfo().then(function(state) {
-    display(state);
-    cb(state);
-  });
-}
 
-function getSchedule(cb) {
-  plug.getScheduleRules().then(function(state) {
-    display(state);
-    cb(state);
-  });
-}
-
-function on() {
-  plug.setPowerState(true).then(display);
-}
-
-function off() {
-  plug.setPowerState(false).then(display);
-}
-
-function toggle() {
-  plug.getSysInfo().then(function(state) {
-    if(state.relay_state === 1) {
-      off();
-    }else {
-      on();
-    }
-  });
-}
 
 app.use('/', router);
 app.listen(port);
-console.log('listening to port ' + port);
+console.log('\nlistening to port ' + port);
